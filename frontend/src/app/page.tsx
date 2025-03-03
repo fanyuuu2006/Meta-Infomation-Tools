e7e42d5fc40ba19687c704bb9b49eacf8e3b9b94
Yorn90104 <bingxiao526@gmail.com>
2025-03-03 20:58:20 +0800

    feat: 讀取 Json檔案之按鈕

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "antd";
+"use client";
+import { FileUploadButton } from "@/components/common/FileUploadButton";
 
 export default function Index() {
   return (
     <>
-      <Button>測試</Button>
+      <FileUploadButton />
     </>
   );
 }
